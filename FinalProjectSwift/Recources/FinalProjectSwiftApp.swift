d22d4880044ae28f271a540d0604f99587c4b8d4
WallacexMcCarthy <89401301+WallacexMcCarthy@users.noreply.github.com>
2022-10-07 08:26:13 -0600

    pulled for wallace

---
+++ FinalProjectSwift/Recources/FinalProjectSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectSwiftApp.swift
+//  FinalProjectSwift
+//
+//  Created by Mccarthy, Wallace on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
