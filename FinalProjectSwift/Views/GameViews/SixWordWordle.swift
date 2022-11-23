277d0673631e757b24fd037e71aeaf74fbfc46bd
Erinnn3746 <111368246+Erinnn3746@users.noreply.github.com>
2022-11-23 12:36:29 -0700

    updated SixWordWordle

--- FinalProjectSwift/Views/GameViews/SixWordWordle.swift
+++ FinalProjectSwift/Views/GameViews/SixWordWordle.swift
@@ -9,16 +9,66 @@ import SwiftUI
 
 struct SixWordWordle: View
 {
+   @ObservedObject var randomWords = WordsDataStore(wordData: loadJSON(from: "wordData") as! [Words])
+    @State private var index = 0
+    @State private var generatedrandomWord = false
+    
+    @State private var randomWord = "Test"
+    @State private var winCount = 0;
+    
     @State private var letterOne = ""
     @State private var letterTwo = ""
     @State private var letterThree = ""
     @State private var letterFour = ""
     @State private var letterFive = ""
     @State private var letterSix = ""
-    @State private var guess = ""
-    @State private var error = false
-    var body: some View
-    {
+    @State private var letterOneColor = Color.white.opacity(0.34)
+    @State private var letterTwoColor = Color.white.opacity(0.34)
+    @State private var letterThreeColor = Color.white.opacity(0.34)
+    @State private var letterFourColor = Color.white.opacity(0.34)
+    @State private var letterFiveColor = Color.white.opacity(0.34)
+    @State private var letterSixColor = Color.white.opacity(0.34)
+    
+    @State private var oneLetterOne = ""
+    @State private var oneLetterTwo = ""
+    @State private var oneLetterThree = ""
+    @State private var oneLetterFour = ""
+    @State private var oneLetterFive = ""
+    @State private var oneLetterSix = ""
+    @State private var oneLetterOneColor = Color.white.opacity(0.34)
+    @State private var oneLetterTwoColor = Color.white.opacity(0.34)
+    @State private var oneLetterThreeColor = Color.white.opacity(0.34)
+    @State private var oneLetterFourColor = Color.white.opacity(0.34)
+    @State private var oneLetterFiveColor = Color.white.opacity(0.34)
+    @State private var oneLetterSixColor = Color.white.opacity(0.34)
+    
+    @State private var twoLetterOne = ""
+    @State private var twoLetterTwo = ""
+    @State private var twoLetterThree = ""
+    @State private var twoLetterFour = ""
+    @State private var twoLetterFive = ""
+    @State private var twoLetterSix = ""
+    @State private var twoLetterOneColor = Color.white.opacity(0.34)
+    @State private var twoLetterTwoColor = Color.white.opacity(0.34)
+    @State private var twoLetterThreeColor = Color.white.opacity(0.34)
+    @State private var twoLetterFourColor = Color.white.opacity(0.34)
+    @State private var twoLetterFiveColor = Color.white.opacity(0.34)
+    @State private var twoLetterSixColor = Color.white.opacity(0.34)
+
+    @State private var threeLetterOne = ""
+    @State private var threeLetterTwo = ""
+    @State private var threeLetterThree = ""
+    @State private var threeLetterFour = ""
+    @State private var threeLetterFive = ""
+    @State private var threeLetterSix = ""
+    @State private var threeLetterOneColor = Color.white.opacity(0.34)
+    @State private var threeLetterTwoColor = Color.white.opacity(0.34)
+    @State private var threeLetterThreeColor = Color.white.opacity(0.34)
+    @State private var threeLetterFourColor = Color.white.opacity(0.34)
+    @State private var threeLetterFiveColor = Color.white.opacity(0.34)
+    @State private var threeLetterSixColor = Color.white.opacity(0.34)
+    
+    
         ZStack
         {
             Color.brown
@@ -107,8 +157,10 @@ struct SixWordWordle: View
     }
 }
 
-struct SixWordWordle_Previews: PreviewProvider {
-    static var previews: some View {
+struct SixWordWordle_Previews: PreviewProvider
+{
+    static var previews: some View
+    {
         SixWordWordle()
     }
 }
